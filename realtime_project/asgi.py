01e553ef1c8d5fd965b0e644c39717ead0c85eea
Lawrance Massanja <massanjal4@gmail.com>
2021-07-14 20:52:56 +0300

    initial commit and done

---
+++ realtime_project/asgi.py
@@ -0,0 +1,21 @@
+"""
+ASGI config for realtime_project project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
+from integer.routing import ws_urlpatterns
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'realtime_project.settings')
+
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(URLRouter(ws_urlpatterns))
+})
